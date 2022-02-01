3a225185cdd64c4e1687e88b581076beacae2142
jx wang <jx@x2v3.com>
2022-02-01 12:50:32 +0800

    config mqtt

--- src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Messaging/MessagingExtensions.cs
+++ src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Messaging/MessagingExtensions.cs
@@ -1,9 +1,38 @@
-﻿namespace SimpleHomeAssistant.ControlCenter.Messaging
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+namespace SimpleHomeAssistant.ControlCenter.Messaging
 {
     /// <summary>
     /// extension methods for MQTT messaging
     /// </summary>
-    public class MessagingExtensions
+    public static class MessagingExtensions
     {
+        public static IServiceCollection AddMqttTopicReceiver(this IServiceCollection services)
+        {
+            services.AddSingleton<IMqttClient>(provider =>
+            {
+                var mqttFactory = new MqttFactory();
+                return mqttFactory.CreateMqttClient();
+            });
+            services.AddSingleton<MqttTopicReceiver>();
+            return services;
+        }
+
+        public static WebApplication UseMqttTopicRecieverAndHandlers(this WebApplication app)
+        {
+            var provider = app.Services.CreateScope().ServiceProvider;
+            var client = provider.GetRequiredService<IMqttClient>();
+            var receiver = provider.GetRequiredService<MqttTopicReceiver>();
+            var config = app.Configuration;
+
+            // config client connection
+            var clientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(config.GetValue<string>("MQTT:ServerAddress"), config.GetValue<int>("MQTT:ServerPort"))
+                .Build();
+            client.ConnectAsync(clientOptions);
+            return app;
+        }
     }
 }
--- src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Messaging/MqttTopicReceiver.cs
+++ src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Messaging/MqttTopicReceiver.cs
@@ -1,4 +1,5 @@
-﻿using MQTTnet;
+﻿using Microsoft.Extensions.Options;
+using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
 
@@ -7,10 +8,15 @@ namespace SimpleHomeAssistant.ControlCenter.Messaging
     public class MqttTopicReceiver
     {
         private IMqttClient _mqttClient;
+        private IMqttClientOptions _mqttClientOptions;
 
-        public MqttTopicReceiver(IMqttClient mqttClient)
+        public MqttTopicReceiver(IOptions<TopicRecieverOptions> options)
         {
-            _mqttClient = mqttClient;
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+            _mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(options.Value.ServerAddress, options.Value.ServerPort)
+                .Build();
         }
 
         public void Start(IEnumerable<IMqttTopicHandler> topicHandlers)
