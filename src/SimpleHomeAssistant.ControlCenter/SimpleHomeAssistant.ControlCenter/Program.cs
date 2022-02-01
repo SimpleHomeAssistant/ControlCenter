14fb15cdd4a9a94e6ebbfff5780314522fa7017d
jx wang <jx@x2v3.com>
2022-02-01 11:13:48 +0800

    add mqtt config

--- src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Program.cs
+++ src/SimpleHomeAssistant.ControlCenter/SimpleHomeAssistant.ControlCenter/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json")
+    .AddEnvironmentVariables("SHA");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -10,9 +13,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
